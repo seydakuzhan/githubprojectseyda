0c3a5fb43e7673005e16294f6067b87469f9d33b
Seyda Kuzhan <seydakuzhan12@gmail.com>
2022-02-11 14:27:19 -0500

    my new classim

--- src/com/company/SecondClass.java
+++ src/com/company/SecondClass.java
@@ -3,6 +3,7 @@ package com.company;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("new page second class");
+        System.out.println("..");
     }
 
 }
