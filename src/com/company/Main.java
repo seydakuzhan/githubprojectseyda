33363fdb2af11678a07282c431097df22a94be23
Seyda Kuzhan <seydakuzhan12@gmail.com>
2022-02-11 14:10:52 -0500

    my second update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1st class");
         System.out.println("2nd step");
+        System.out.println("third step");
+        System.out.println("fourth class");
     }
 }
 
